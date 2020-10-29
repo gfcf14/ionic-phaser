e4cbb08d35c1d6d3888e84ce0604f46a17edc6ff
Carlos(gfcf14) <carloscuba014@gmail.com>
2020-10-29 15:56:08 -0400

    add functions and basic physics for a sidescroller arcade game

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -14,15 +14,24 @@ export class HomePage implements OnInit, OnDestroy {
 
   constructor(private plt: Platform) {
     this.config = {
-      height: this.plt.height(),
+      // height: this.plt.height(),
+      height: 600,
       parent: 'gameContainer',
+      physics: {
+        default: 'arcade',
+        arcade: {
+          gravity: { y: 300 },
+          debug: false
+        }
+      },
       scale: {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH
       },
       scene: GameScene,
       type: Phaser.AUTO,
-      width: this.plt.width()
+      // width: this.plt.width()
+      width: 800
     };
   }
 
